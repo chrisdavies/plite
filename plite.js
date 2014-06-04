4ef3d4ea3b75bd238f04f3c5a4d428c1dbdc16a8
Christopher Davies <chris.davies@sparcedge.com>
2014-06-04 10:25:50 -0400

    Adding plite tests, fixing resulting bugs.

--- plite.js
+++ plite.js
@@ -1,80 +1,80 @@
 function Plite() {
 
-        var result,
-            completed,
-            thenFn,
-            catchFn = function (err) { console.log(err); },
-            finallyFn = function () { },
-            me;
+    var result,
+        completed,
+        thenFn,
+        catchFn = function (err) { console.log(err); return err; },
+        finallyFn = function () { },
+        me;
 
-        function then(fn) {
-            var prevThen = thenFn || function (o, fn) { fn && fn(o); };
+    function then(fn) {
+        var prevThen = thenFn || function (o, fn) { fn && fn(o); };
 
-            thenFn = function (o, then) {
-                prevThen(o, function (res) {
-                    result = fn(res);
+        thenFn = function (o, then) {
+            prevThen(o, function (res) {
+                result = fn(res);
 
-                    if (!then) {
-                        completed = true;
-                        finallyFn(result);
-                    } else if (result.then) {
-                        result.then(then).catch(function (err) {
-                            reject(err);
-                        });
-                    } else {
-                        then(result);
-                    }
-                });
-            };
+                if (!then) {
+                    completed = true;
+                    finallyFn(result);
+                } else if (result.then) {
+                    result.then(then).catch(function (err) {
+                        reject(err);
+                    });
+                } else {
+                    then(result);
+                }
+            });
+        };
 
-            completed && resolve(result);
+        completed && resolve(result);
 
-            return me;
-        }
+        return me;
+    }
 
-        function _catch (fn) {
-            catchFn = fn;
-            return me;
-        }
+    function _catch (fn) {
+        catchFn = fn;
+        return me;
+    }
 
-        function _finally (fn) {
-            finallyFn = fn;
-            completed && fn(result);
-            return me;
-        }
+    function _finally (fn) {
+        finallyFn = fn;
+        completed && fn(result);
+        return me;
+    }
         
-        function resolve (obj) {
-            result = obj;
+    function resolve (obj) {
+        result = obj;
 
-            if (!(completed = (thenFn === undefined))) {
-                var fn = thenFn;
-                thenFn = undefined;
-                fn(obj);
-            }
-        }
-
-        function reject (err) {
-            result = err;
-            then = function () { return this; };
-            catchFn(err);
-            finallyFn(err);
-
-            function callInstantly(fn) {
-                fn(this.result);
-                return this;
-            }
-            
-            me.catch = me.finally = function (fn) {
-                fn(result);
-                return this;
-            }
-        }
-
-        return me = {
-            'catch': _catch,
-            'finally': _finally,
-            then: then,
-            reject: reject,
-            resolve: resolve
+        if (!(completed = (thenFn === undefined))) {
+            var fn = thenFn;
+            thenFn = undefined;
+            fn(obj);
         }
     }
+
+    function reject (err) {
+        result = err;
+        then = function () { return this; };
+        result = catchFn(result);
+        finallyFn(result);
+
+        function callInstantly(fn) {
+            fn(result);
+            return this;
+        }
+            
+        me.catch = me.finally = function (fn) {
+            result = fn(result);
+            return this;
+        }
+    }
+
+    return me = {
+        'catch': _catch,
+        'finally': _finally,
+        then: then,
+        reject: reject,
+        resolve: resolve
+    }
+}
